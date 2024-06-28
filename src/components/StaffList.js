bf652d3aa81bfea1e6a6da92fca53d2d985b7c2d
nitinsatya2520 <nitinsatya6561@gmail.com>
2024-06-28 16:13:34 +0530

    Update StaffList.js

--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -10,12 +10,14 @@ const StaffList = () => {
             
             {staffData.map(staff => (
                 <div key={staff.id} className="staff-card">
+                    <a href={staff.link}>
                     <img
                         src={require(`../assets/images/${staff.image}`)} // Dynamic image import
                         alt={staff.name}
                         onError={(e) => { e.target.onerror = null; e.target.src = '/path_to_placeholder_image.jpg' }} // Handling image load errors
                         className="staff-image"
                     />
+                    </a>
                     <h3>{staff.name}</h3>
                     <h4>{staff.position}</h4>
                     <p>{staff.bio}</p>
